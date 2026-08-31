d51d5d52d1aa9a2d0703d13f34f4cec85c6ad8d6
Leshka <tsysa1607@gmail.com>
2026-08-31 11:48:23 +0200

    migrate

--- my_project/shop/models.py
+++ my_project/shop/models.py
@@ -12,42 +12,85 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to="shop/", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.PROTECT, 
+        related_name="products")
+
+    def __str__(self):
+        return self.name
 
 class Cart(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="cart")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    quantity = models.PositiveIntegerField
+    cart = models.ForeignKey(
+        Cart, 
+        on_delete=models.CASCADE, 
+        related_name="items")
+    product = models.ForeignKey(
+        Product, 
+        on_delete=models.PROTECT, 
+        related_name="cart_item")
+    quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity} sht."
+
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="orders")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     sum_order = models.DecimalField(max_digits=11, decimal_places=2)
 
+    def __str__(self):
+        return f"Order {self.id}"
+
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    quantity = models.PositiveIntegerField
+    order = models.ForeignKey(
+        Order, 
+        on_delete=models.CASCADE, 
+        related_name="items")
+    product = models.ForeignKey(
+        Product, 
+        on_delete=models.PROTECT, 
+        related_name="order_item")
+    quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity}"
+
 class Purchase(models.Model):
-    orderItem = models.OneToOneField(OrderItem, on_delete=models.PROTECT)
-    sum_order = 
+    orderItem = models.OneToOneField(
+        OrderItem, 
+        on_delete=models.CASCADE, 
+        related_name="purchase")
+    paid_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def sum_order(self):
+        return self.order_item.price * self.order_item.quantity
+
+    def __str__(self):
+        return f"Order #{self.id}"
 
 
 
